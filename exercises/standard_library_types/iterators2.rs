e7924e493084b2dc45171446e314d4386c31064e
UsamaHameed <usamatech2@gmail.com>
2021-03-26 23:32:06 +0100

    iterators experiments

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -9,13 +9,63 @@
 
 // I AM NOT DONE
 
-pub fn capitalize_first(input: &str) -> String {
-    let mut c = input.chars();
-    match c.next() {
-        None => String::new(),
-        Some(first) => first.collect::<String>() + c.as_str(),
-    }
+// pub fn capitalize_first(input: &str) -> String {
+//     let mut c = input.chars();
+//     match c.next() {
+//         None => String::new(),
+//         Some(first) => String::from(first.to_uppercase().to_string() + &input[1..]),
+//     }
+// }
+
+pub trait Input<'a> { 
+    fn to_vec(self) -> Vec<&'a str>; 
 }
+
+impl<'a> Input<'a> for &'a str {
+   fn to_vec(self) -> Vec<&'a str> { vec![self] }
+}
+
+impl<'a> Input<'a> for Vec<&'a str> {
+   fn to_vec(self) -> Vec<&'a str> { self }
+}
+
+pub fn capitalize_first<'a>(input: impl Input<'a>) -> Vec<String> {
+    let words = input.to_vec().iter().map(|word| { 
+        let mut c = word.chars();
+        match c.next() {
+            None => String::new(),
+            Some(first) => String::from(first.to_uppercase().to_string() + &word[1..]),
+        }
+    }).collect::<Vec<String>>();
+
+    words
+}
+
+// struct OneOrMore<'a>(Vec<&'a str>);
+
+// impl<'a> From<&'a str> for OneOrMore<'a> {
+//     fn from(v: &'a str) -> Self {
+//         Self(vec![v])
+//     }
+// }
+
+// impl<'a> From<Vec<&'a str>> for OneOrMore<'a> {
+//     fn from(v: Vec<&'a str>) -> Self {
+//         Self(v)
+//     }
+// }
+
+// pub fn capitalize_first<'a, T>(input: T) -> T where T: Into<OneOrMore<'a>> {
+//     let words = input.iter().map(|word| { 
+//         let mut c = word.chars();
+//         match c.next() {
+//             None => String::new(),
+//             Some(first) => String::from(first.to_uppercase().to_string() + &word[1..]),
+//         }
+//     }).collect::<Vec<String>>();
+
+//     words
+// }
 
 #[cfg(test)]
 mod tests {
@@ -25,26 +75,26 @@ mod tests {
     // Tests that verify your `capitalize_first` function implementation
     #[test]
     fn test_success() {
-        assert_eq!(capitalize_first("hello"), "Hello");
+        assert_eq!(capitalize_first("hello"), vec!["Hello"]);
     }
 
     #[test]
     fn test_empty() {
-        assert_eq!(capitalize_first(""), "");
+        assert_eq!(capitalize_first(""), vec![""]);
     }
 
     // Step 2.
     #[test]
     fn test_iterate_string_vec() {
         let words = vec!["hello", "world"];
-        let capitalized_words: Vec<String> = // TODO
+        let capitalized_words: Vec<String> = capitalize_first(words);
         assert_eq!(capitalized_words, ["Hello", "World"]);
     }
 
     #[test]
     fn test_iterate_into_string() {
         let words = vec!["hello", " ", "world"];
-        let capitalized_words = // TODO
-        assert_eq!(capitalized_words, "Hello World");
+        let capitalized_words = capitalize_first(words);
+        assert_eq!(capitalized_words, ["Hello World"]);
     }
 }
